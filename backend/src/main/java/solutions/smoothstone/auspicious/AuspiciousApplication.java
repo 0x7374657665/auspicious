40661cac387ace2b332ac4d53dd9eb4f4dee36ae
Steve Russell <0x7374657665@gmail.com>
2018-08-24 21:28:36 -0400

    initial commit

---
+++ backend/src/main/java/solutions/smoothstone/auspicious/AuspiciousApplication.java
@@ -0,0 +1,12 @@
+package solutions.smoothstone.auspicious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuspiciousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuspiciousApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/solutions/smoothstone/auspicious/AuspiciousApplicationTests.java
@@ -0,0 +1,16 @@
+package solutions.smoothstone.auspicious;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuspiciousApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
